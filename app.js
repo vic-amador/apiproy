6ca9c25716975baa34e19a6a6c884869c328e4c8
vic-amador <83994187+vic-amador@users.noreply.github.com>
2023-10-02 19:46:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,26 +8,14 @@ const db = require('./db');
 
 // Ruta para obtener los reportes
 app.get('/Reportes', (req, res) => {
-  db.query('SELECT reporte_id, firstName, lastName, location_report, image FROM report', (err, results) => {
+  db.query('SELECT reporte_id, firstName, lastName, location_report, TO_BASE64(image) AS imageBase64 FROM report', (err, results) => {
     if (err) {
       console.error('Error al consultar la base de datos: ' + err);
       res.status(500).json({ error: 'Error al obtener los reportes' });
       return;
     }
 
-    // Modificar la respuesta para convertir la imagen a base64
-    const resultsWithBase64Image = results.map((result) => {
-      if (result.image && result.image.data) {
-        // Convierte los datos de la imagen en base64
-        result.image = {
-          data: Buffer.from(result.image.data).toString('base64'),
-          type: 'image/jpeg' // Cambia el tipo de imagen según corresponda
-        };
-      }
-      return result;
-    });
-
-    res.json(resultsWithBase64Image);
+    res.json(results);
   });
 });
 
